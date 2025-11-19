fbaf057c80336f2f59f1dea43fb427acb293a9ed
ShubhamDudhbhate <shubhamdudhbhate2006@gmail.com>
2025-11-19 21:25:47 +0530

    Update DAA5.2.cpp

--- DAA5.2.cpp
+++ DAA5.2.cpp
@@ -21,7 +21,7 @@ void dfs(node* root,string s){
         m.insert({root->c,s});
         return;}
     
-    dfs(root->left , s + "1");
+    dfs(root->left , s + "0");
     dfs(root->right, s + "2");
 }
 
@@ -93,5 +93,6 @@ int main() {
     cout << "Percentage of compression : " << 100-resultOfCompression << endl;
     return 0;
 }
+
 
 
